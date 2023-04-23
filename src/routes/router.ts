67ca303a2eb3db602d2c76b438864ccc7100046c
S. O <83116574+PentaMine@users.noreply.github.com>
2023-04-23 18:38:44 +0200

    Update router.ts

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,7 +4,6 @@ import jwtMiddleware from "../middleware/jwtMiddleware";
 import {createSession, exitSession, joinSession} from "../controller/sessionControler";
 import {getNNewestSessions} from "../service/session";
 import okResponse from "../responses/okResponse";
-import { getLeaderBoard, registerSubmiter, submitScore } from "../controller/kuracKlikerController";
 
 const router = Router();
 
